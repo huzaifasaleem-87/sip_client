35eed55a8a7c900875a2ed0d361a4febc98187fe
Huzaifa Saleem <huzaifa.saleem@xflowresearch.com>
2023-02-08 21:31:05 +0500

    Removed ci dir, will create one later when I learn basic ci/cd

--- ci/check_codestyle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# this script is expected to by executed by a build server from the top level of the project
-
-set -eo pipefail
-
-ci/run-clang-format.py --clang-format-executable clang-format-15 -r main/ components/ native/
